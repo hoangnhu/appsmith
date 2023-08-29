db85413fe2b50d5952f5fb0e17764e964bb8ec2c
Nguyễn Như Hoàng <hoangnn@mindx.edu.vn>
2023-08-29 18:58:26 +0000

    First Commit

--- pages/Home/jsobjects/utils/utils.js
+++ pages/Home/jsobjects/utils/utils.js
@@ -4,9 +4,16 @@ export default {
 		if (hostName === "localhost") {
 			storeValue('URL_API', 'https://lms-api-dev.mindx.edu.vn/');
 		} else {
-			storeValue('URL_API', 'Link to production api');
+			storeValue('URL_API', 'https://lms-api-dev.mindx.edu.vn/');
 		}
-
 		return appsmith.store.URL_API;
+	},
+
+	getCourses: async () => {
+		const courses = await getCourses.run();
+		if(!courses.data.courses) {
+			navigateTo("Login");
+		}
+		return courses;
 	}
 }
