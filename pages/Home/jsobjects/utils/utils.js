86003b2041f49543268149150be6a46f431cc0b8
hoang hoangn <hoangnn@mindx.edu.vn>
2023-09-12 19:52:19 +0000

    First Commit

--- pages/Home/jsobjects/utils/utils.js
+++ pages/Home/jsobjects/utils/utils.js
@@ -1,19 +1,35 @@
 export default {
-	setEnv: () => {
-		const hostName = appsmith.URL.hostname;
-		if (hostName === "localhost") {
-			storeValue('URL_API', 'https://lms-api-dev.mindx.edu.vn/');
-		} else {
-			storeValue('URL_API', 'https://lms-api-dev.mindx.edu.vn/');
+	setEnv: async () => {
+		const getEnvQuery = await getEnv.run();
+		if(getEnvQuery.length > 0) {
+			storeValue('BASE_API_URL_LMS', getEnvQuery[0].BASE_API_URL_LMS);
+			storeValue('BASE_API_URL', getEnvQuery[0].BASE_API_URL);
+			storeValue('BASE_WEB_URL', getEnvQuery[0].BASE_WEB_URL);
 		}
-		return appsmith.store.URL_API;
+		return appsmith.store.BASE_WEB_URL;
 	},
 
-	getCourses: async () => {
-		const courses = await getCourses.run();
-		if(!courses.data.courses) {
-			navigateTo("Login");
+	setTokenHash: async () => {
+		storeValue('TOKEN_HASH', appsmith.URL.queryParams.token_hash);
+		return appsmith.store.TOKEN_HASH;
+	},
+
+	setAuthToken: async () => {
+		const getCustomTokenResult = await getCustomToken.run();
+		if (getCustomTokenResult?.errors && getCustomTokenResult?.errors[0].extensions.code === 'NOT_AUTHENTICATED') {
+			storeValue('CUSTOM_TOKEN', '');
+			// navigateTo(appsmith.store.BASE_WEB_URL);	
+		} else {
+			storeValue('CUSTOM_TOKEN', getCustomToken.data.data.users.getCustomToken.customToken);
 		}
-		return courses;
+		return appsmith.store.CUSTOM_TOKEN;
+	},
+
+	verifyCustomToken: async () => {
+		const getTokenResult = await verifyCustomToken.run();
+		if (getTokenResult) {
+			storeValue('TOKEN', getTokenResult.idToken);
+		}
+		return appsmith.store.TOKEN;
 	}
 }
